db94e05fdaeb6a4fcb2b65d8d2a541ae8f14132e
nathannoodler29 <nathannoodler29@gmail.com>
2017-02-17 15:27:20 +0000

    Created a new secondary category file for my applicaiton, which states different kinds of acholic drinks, then a user clicks this then it'll display more drinks based on the category.

---
+++ app/src/main/java/coffee/prototype/android/cleandrinksapplication/DrinksCategoryTypes.java
@@ -0,0 +1,13 @@
+package coffee.prototype.android.cleandrinksapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrinksCategoryTypes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drinks_category_types);
+    }
+}
