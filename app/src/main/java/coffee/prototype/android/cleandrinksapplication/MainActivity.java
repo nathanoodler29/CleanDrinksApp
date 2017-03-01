264d9e81aca289afab5c9156d0be1bb323fff2ff
nathannoodler29 <nathannoodler29@gmail.com>
2017-03-01 20:56:53 +0000

    I've added validaiton to the main_activity which is the login screen. i've also refactored the layout code and improved the style of the login page.

--- app/src/main/java/coffee/prototype/android/cleandrinksapplication/MainActivity.java
+++ app/src/main/java/coffee/prototype/android/cleandrinksapplication/MainActivity.java
@@ -1,23 +1,20 @@
 package coffee.prototype.android.cleandrinksapplication;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    private CallbackManager callbackManager;
-    private TextView sigin;
-    private LoginButton loginButton;
+    private EditText emailAddressInput;
+    private EditText passwordInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +24,112 @@ public class MainActivity extends AppCompatActivity {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
+        emailAddressInput = (EditText) findViewById(R.id.email_address_field);
+        passwordInput = (EditText) findViewById(R.id.password_field);
+
+        validateEmailField();
+        validatePasswordField();
+
+    }
+
+    /**
+     * Intalises a Toast object and then displays text.
+     *
+     * @param toastText Uses the text passed, and is displayed by the toast.
+     */
+    public void createToastWithText(String toastText) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, toastText, duration);
+        toast.show();
+    }
+
+    /**
+     * This method checks that a user isn't passing in illegal special characters.
+     * or empty strings or invalid emails.
+     */
+    public void validateEmailField() {
+
+        emailAddressInput.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            //Text watcher, monitors what text is typed by the user
+            @Override
+            public void afterTextChanged(Editable s) {
+                //Converts the input from a user
+                String userInput = emailAddressInput.getText().toString();
+                if (userInput.isEmpty()) {
+                    emailAddressInput.setError("Please don't leave blank ");
+                } else if (userInput.contains("*") | userInput.contains("\0") | userInput.contains("\'")
+                        | userInput.contains("\0")
+                        | userInput.contains("\"") | userInput.contains("\b") | userInput.contains("\n")
+                        | userInput.contains("\r") | userInput.contains("\t") | userInput.contains("\t")
+                        | userInput.contains("\\") | userInput.contains("%")) {
+
+                    emailAddressInput.setError("Special characters can't be used");
+
+//                Regex from Google regex checker
+
+                } else if (userInput.matches("^(\\w[-._+\\w]*\\w@\\w[-._\\w]*\\w\\.\\w{2,3})$")) {
+                    createToastWithText("Valid Email");
+                } else if (!userInput.matches("^(\\w[-._+\\w]*\\w@\\w[-._\\w]*\\w\\.\\w{2,3})$")) {
+                    emailAddressInput.setError("Please include a valid email");
+                }
+            }
+        });
+    }
+
+    public void validatePasswordField() {
+
+        passwordInput.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            //Text watcher, monitors what text is typed by the user
+            @Override
+            public void afterTextChanged(Editable s) {
+                //Converts the input from a user
+                String userInput = passwordInput.getText().toString();
+                if (userInput.isEmpty()) {
+                    passwordInput.setError("Please don't leave blank ");
+                } else if (userInput.contains("*") | userInput.contains("\0") | userInput.contains("\'")
+                        | userInput.contains("\0")
+                        | userInput.contains("\"") | userInput.contains("\b") | userInput.contains("\n")
+                        | userInput.contains("\r") | userInput.contains("\t") | userInput.contains("\t")
+                        | userInput.contains("\\") | userInput.contains("%")) {
+                    passwordInput.setError("Special characters can't be used");
+
+                } else if (userInput.length()<=3) {
+                    passwordInput.setError("Password must be longer than four characters");
+                }else if (!userInput.matches(".*\\d+.*")) {
+                    passwordInput.setError("Password Needs to contain 1 number");
+                }
+
+            }
+        });
+
+
+
 
 
     }
 }
+
+
---
+++ app/src/main/java/coffee/prototype/android/cleandrinksapplication/achievement_gallery.java
@@ -0,0 +1,13 @@
+package coffee.prototype.android.cleandrinksapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class achievement_gallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achievement_gallery);
+    }
+}
