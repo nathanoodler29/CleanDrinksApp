fb8929cb41d971bdb86f656682b36047a3d987b9
nathannoodler29 <nathannoodler29@gmail.com>
2017-03-27 17:09:32 +0100

    Added goal prompt, user can close and open the prompt. The story it relates to is #140 in the mean time, i've commented out part of the hardcoded values of the goal activity, this is so when I implement the goal table, i'll be able to then display records from the table rather than hard coded.  Added allow the use of prompts in the android manifest file also.

--- app/src/main/java/coffee/prototype/android/cleandrinksapplication/GoalActivity.java
+++ app/src/main/java/coffee/prototype/android/cleandrinksapplication/GoalActivity.java
@@ -1,13 +1,62 @@
 package coffee.prototype.android.cleandrinksapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class GoalActivity extends AppCompatActivity {
+
+
+    private Button addgoalbutton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goal);
+        //Creates a pop up dialog when a user clicks add goal.
+        createGoalPrompt();
     }
+
+    public void createGoalPrompt() {
+        addgoalbutton = (Button) findViewById(R.id.adddrinkgoal);
+
+        addgoalbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //reference to http://www.mkyong.com/android/android-custom-dialog-example/
+                final Dialog dialog = new Dialog(GoalActivity.this);
+
+                dialog.setContentView(R.layout.add_goal_dialog);
+
+
+                EditText numOfLitesDialog = (EditText) findViewById(R.id.numberOfLitres);
+
+                EditText startTimeDialog = (EditText) findViewById(R.id.starttime);
+
+                EditText endTimeDialog = (EditText) findViewById(R.id.endtime);
+
+                Button dialogCloseButton = (Button) dialog.findViewById(R.id.canceldialog);
+
+
+                dialogCloseButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+
+                dialog.show();
+
+
+            }
+        });
+
+    }
+
+
 }
