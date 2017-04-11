8d0c91cf49b54a7ef87949b3c9748679a560fdb4
nathannoodler29 <nathannoodler29@gmail.com>
2017-04-11 13:45:27 +0100

    In future will migrate queries from activity helper in this class.

---
+++ app/src/main/java/coffee/prototype/android/cleandrinksapplication/data/DBQueryHelper.java
@@ -0,0 +1,13 @@
+package coffee.prototype.android.cleandrinksapplication.data;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+/**
+ * created by noodle on 04/04/2017.
+ */
+
+public class DBQueryHelper {
+
+}
