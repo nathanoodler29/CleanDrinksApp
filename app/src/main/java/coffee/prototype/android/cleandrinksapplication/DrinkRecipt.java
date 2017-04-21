70074275aad658b94c3e53cd6828cb08ac4348de
nathannoodler29 <nathannoodler29@gmail.com>
2017-04-21 16:06:57 +0100

    Implemented #237 If no drinks are in the adapter, display a card stating no drinks have been added: #226 Drinks receipt

--- app/src/main/java/coffee/prototype/android/cleandrinksapplication/DrinkRecipt.java
+++ app/src/main/java/coffee/prototype/android/cleandrinksapplication/DrinkRecipt.java
@@ -2,8 +2,11 @@ package coffee.prototype.android.cleandrinksapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,8 +19,10 @@ public class DrinkRecipt extends AppCompatActivity {
     private ActivityHelper helper= new ActivityHelper();
     private ArrayList<Drink>drinksRecipt = new ArrayList<>();
     private DrinkReciptAdapter mAdapter;
-
-
+    private CardView cardViewHeading;
+    private CardView cardViewBottom;
+    private TextView noDrinksSetHeading;
+    private TextView noDrinksSetParagraph;
 
 
 
@@ -31,7 +36,22 @@ public class DrinkRecipt extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
         mAdapter = new DrinkReciptAdapter(this, helper.populateDrinksReciptAdatper(getApplicationContext()));
+        cardViewHeading = (CardView) findViewById(R.id.no_values_drink_recipt_heading);
+        cardViewBottom = (CardView) findViewById(R.id.no_drinks_set_card);
+        noDrinksSetHeading = (TextView) findViewById(R.id.no_drinks_set_parent_heading);
+        noDrinksSetParagraph = (TextView) findViewById(R.id.onstrike_paragraph_text);
+
         recyclerView.setAdapter(mAdapter);
+        if (mAdapter.getItemCount()==0){
+            cardViewHeading.setVisibility(View.VISIBLE);
+            cardViewBottom.setVisibility(View.VISIBLE);
+            noDrinksSetHeading.setVisibility(View.VISIBLE);
+            noDrinksSetParagraph.setVisibility(View.VISIBLE);
+        }else{
+            cardViewHeading.setVisibility(View.GONE);
+            cardViewBottom.setVisibility(View.GONE);
+            noDrinksSetHeading.setVisibility(View.GONE);
+            noDrinksSetParagraph.setVisibility(View.GONE);
+        }
     }
 }
-//populateDrinksReciptAdatper
